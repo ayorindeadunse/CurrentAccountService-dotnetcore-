0badc7a793025d05e61af1c8f0bbdde61e0cb9cb
Ayorinde Adunse <adunseayorinde@gmail.com>
2023-07-03 02:14:59 +0100

    stashing changes

--- CurrentAccountService/Services/Core/AccountService.cs
+++ CurrentAccountService/Services/Core/AccountService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CurrentAccountService.Entities;
+using CurrentAccountService.Repositories;
 using CurrentAccountService.Services.Abstract;
 
 namespace CurrentAccountService.Services.Core
@@ -7,13 +8,16 @@ namespace CurrentAccountService.Services.Core
     public class AccountService : IAccountService
     {
         private readonly Dictionary<string, Account> _accounts = new Dictionary<string, Account>();
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
 
         public Account GetAccountInformation(string customerID)
         {
-            if (_accounts.TryGetValue(customerID, out var account))
-                return account;
-
-            return null;
+            return _accountRepository.GetAccountsByCustomerID(customerID)?.FirstOrDefault();
         }
 
         public Account OpenAccount(string customerID, decimal initialCredit)
